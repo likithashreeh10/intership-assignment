0fe0a7e875759668e6845d98fb4a4f2bf81acd79
Likithashree H <hlikithashree@gmail.com>
2026-02-06 11:29:00 +0530

    Initial commit

---
+++ assign2.py
@@ -0,0 +1,41 @@
+#secound assignment
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def display_person(self):
+        print(f"Name: {self.name}")
+
+
+class Student(Person):
+    def __init__(self, name, student_id):
+        Person.__init__(self, name)
+        self.student_id = student_id
+
+    def display_student(self):
+        print(f"Student ID: {self.student_id}")
+
+
+class SportsPlayer(Person):
+    def __init__(self, name, sport_name):
+        Person.__init__(self, name)
+        self.sport_name = sport_name
+
+    def display_sports_player(self):
+        print(f"Sport: {self.sport_name}")
+
+class CollegeStudent(Student, SportsPlayer):
+    def __init__(self, name, student_id, sport_name, college_name):
+        Student.__init__(self, name, student_id)
+        SportsPlayer.__init__(self, name, sport_name)
+        self.college_name = college_name
+
+    def display_college_student(self):
+        self.display_person()
+        self.display_student()
+        self.display_sports_player()
+        print(f"College: {self.college_name}")
+
+
+college_student = CollegeStudent("Naina","CS101","Badminton","ABC college")
+college_student.display_college_student()
